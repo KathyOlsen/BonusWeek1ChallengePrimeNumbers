b8d5d56d75e87268af84cd261ce57bc1d574ddda
KathyOlsen <katherinekolsen@verizon.net>
2019-09-06 15:12:08 -0400

    Java 8-Week Boot Camp Week 1 Bonus Challenge - Both Options

--- src/IsItPrime.java
+++ src/IsItPrime.java
@@ -1,15 +1,27 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class IsItPrime {
     public static void main(String[] args){
 
-        int num;
+        int num=0;
         int counter=2;
+        String random="";
 
         Scanner key = new Scanner(System.in);
 
-        System.out.println("Please enter a number greater than zero: "); //Option 1
-        num = key.nextInt();
+        System.out.println("Would you like to generate a random number? (Y/N)");
+        random = key.next();
+        if(random.equalsIgnoreCase("Y")){
+            Random r = new Random();
+            num = 1 + r.nextInt(10000);
+            System.out.println("Your random number is " + num);
+        }else if(random.equalsIgnoreCase("N")){
+            System.out.println("Please enter a number greater than zero: ");
+            num = key.nextInt();
+        }else{
+            System.out.println("");
+        }
 
         if(num == 1){
             System.out.println(num + " is not prime");
@@ -30,7 +42,7 @@ public class IsItPrime {
                 }
             }
         }else{
-            System.out.println("Invalid number.");
+            System.out.println("Invalid response.");
         }
     }
 }
